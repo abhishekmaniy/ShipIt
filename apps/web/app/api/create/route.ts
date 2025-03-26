4e25fef6db80a571f1785d7dc4f97b98e8a71df1
abhishekmaniy <abhishekmaniyar502@gmail.com>
2025-03-26 17:55:37 +0530

    removing req object from route.ts

--- apps/web/app/api/create/route.ts
+++ apps/web/app/api/create/route.ts
@@ -1,14 +1,14 @@
 import { prismaClient } from 'db/client'
 import { NextResponse } from 'next/server'
 
-export const POST = async (req: Request) => {
+export const POST = async () => {
   try {
     await prismaClient.user.create({
       data: {}
     })
     return NextResponse.json({ message: 'Success' }, { status: 200 })
   } catch (error) {
-    return NextResponse.json({ message: 'Error' }, { status: 500 })
+    return NextResponse.json({ message: error }, { status: 500 })
   } finally {
     await prismaClient.$disconnect()
   }
